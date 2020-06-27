c391d0ddd449028d3d56c91416539af002954fda
Victoria Kee <victoriakee95@gmail.com>
2020-06-26 19:23:20 -0500

    Change colors with time slots

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const hrs = today.getHours();
   document.writeln("<b>");
   if(hrs <= 12){
     document.write("Good morning, " + name + "!");
+    document.body.style.backgroundColor = "#F1E85F";
     } else if (hrs <= 18) {
     document.write("Good afternoon, " + name + "!");
+    document.body.style.backgroundColor = "#5F68F1";
     } else {
       document.write("Good evening, " + name + "!");
+      document.body.style.color = "white";
+      document.body.style.backgroundColor = "#131430";
     }
